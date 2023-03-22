50d930b1725a59b208521eb0e1cb55ef9a4ea2ad
Tmin1021 <ngngocduytan8604@gmail.com>
2023-03-22 15:08:31 +0700

    Structure2

--- System/System/Structure.h
+++ System/System/Structure.h
@@ -1,9 +1,34 @@
 #pragma once
 #include <string>
 using namespace std;
+struct Date{
+	int day;
+	int month;
+	int year;
+};
 struct Staff {
 	string name;
 	string username;
 	string password;
 	Staff* staffNext;
 };
+struct Class{
+    string name;
+    int numStu;
+    Student* stuHead;
+	Class* classNext;
+};
+struct SchoolYear{
+    int year;
+    Class* classHead;
+    SchoolYear* sYearNext;
+};
+struct Semester{
+	//Semester 1, 2 or 3?
+    int num;
+	//Start date and end date
+    Date startDate, endDate;
+    Course* courseHead;
+};
+
+
