75ccc306fc9b1fea16606fa22cde159fd20a1d4a
lamanh-lavain <lavainse@gmail.com>
2023-03-28 23:42:24 +0700

    Update Structure

--- System/System/Structure.h
+++ System/System/Structure.h
@@ -4,20 +4,28 @@
 
 using namespace std;
 
-struct Date{
-	int day;
-	int month;
-	int year;
+struct Date {
+	string day;
+	string month;
+	string year;
 };
 
 struct Staff {
+	//elements
 	string name;
 	string username;
 	string password;
 	Staff* staffNext;
+
+	//functions
+	void exportListStuToCSV(Course* course);
+	void importScoreboard(Course* course);
+	void viewScoreboard(StuInCourse* s);
+	void updateRes(Course* p);
 };
 
 struct Student {
+	//detail
 	string No;
 	string StuID;
 	string firstName;
@@ -27,31 +35,62 @@ struct Student {
 	string socialID;
 	string username;
 	string password;
+
+	//node
 	Student* stuNext = NULL;
+	Class* stuClass = NULL;
+
+	//function
+	void viewScoreBoard(SchoolYear* sy, Student* p, int numSm);
+	bool selectCourse(SchoolYear* sy, Student* stu, int numSm);
 };
 
-struct Class{
-    string name;
-    int numStu;
-    Student* stuHead;
+struct Class {
+	//detail
+	string name;
+	int numStu;
+
+	//node
+	Student* stuHead;
 	Class* classNext;
 };
 
-struct SchoolYear{
+struct SchoolYear {
+	//detail
 	string name; //ex: 2022-2023
-    Class* classHead;
-    SchoolYear* sYearNext;
+
+	//node
+	Class* classHead;
+	SchoolYear* sYearNext;
+
+	//array
 	Semester sm[3];
 };
 
-struct Semester{
+struct Semester {
+	//detail
 	Date start;
 	Date end;
 	bool state; //if 1 - accessible
+
+	//node
 	Course* courseHead;
 };
 
+struct StuInCourse {
+	//detail 
+	float totalM;
+	float finalM;
+	float midM;
+	float otherM;
+
+	//node 
+	Student* stuInClass = NULL; //point to the node Student in a Class 
+	StuInCourse* stuNext = NULL; //next of linkedlist
+};
+
 struct Course {
+	//detail
 	string ID;
 	string name;
 	string className;
@@ -61,7 +100,9 @@ struct Course {
 	int numStudent;
 	string day; //(MON / TUE / WED / THU / FRI / SAT)
 	string session; //(S1(7:30), S2(09:30), S3(13:30), S4(15:30)
-	Student* stuHead;
+
+	//node
+	StuInCourse* stuHead;
 	Course* courseNext;
 };
 
