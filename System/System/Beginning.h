c32de62b510ffe258b79f4cca5989cbb18abaad9
nghednh <nghednh@gmail.com>
2023-03-29 21:18:00 +0700

    update Beginning.h

--- System/System/Beginning.h
+++ System/System/Beginning.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Structure.h"
 
+
+void chooseRole();
+void login();
+void viewProfile(Staff* staff);
 void inputDate(Date& datee);
 void inputYear(SchoolYear*& year);
 void inputCourse(Course*& course);
