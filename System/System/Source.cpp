bd1215d8594f3cfa689221c12a89048c3e665535
nghednh <nghednh@gmail.com>
2023-03-29 20:45:25 +0700

    inputSchoolYear

--- System/System/Source.cpp
+++ System/System/Source.cpp
@@ -1,8 +1,33 @@
 #include "Structure.h"
 #include <iostream>
+#include <string.h>
 
 using namespace std;
 
 void inputDate(Date& datee) {
 	cin >> datee.day >> datee.month >> datee.year;
+}
+void inpYears(SchoolYear*& year) {
+    SchoolYear* cur = year = nullptr;
+    int n;
+    cin >> n;
+    cin.get();
+    while (n--) {
+        if (!year) {
+            year = new SchoolYear;
+            cur = year;
+        }
+        else {
+            cur->yearNext = new SchoolYear;
+            cur = cur->yearNext;
+        }
+        int numberClass;
+        cin >> numberClass;
+        while (numberClass--) {
+            Class* tem = new Class;
+            cin >> tem->name;
+            tem->classNext = cur->classHead;
+            cur->classHead = tem;
+        }
+    }
 }
