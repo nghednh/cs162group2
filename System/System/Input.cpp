27627b2ab41c73a37367ce4580c72dcf3b63efaa
kknguyen214 <128380887+kknguyen214@users.noreply.github.com>
2023-04-05 00:01:18 +0700

    Input all files

--- System/System/Input.cpp
+++ System/System/Input.cpp
@@ -13,46 +13,62 @@ struct User {
 };
 
 void processCSVFile(const path& file_path) {
-    ifstream file(file_path.c_str()); // mở file
-    if (!file.good()) { // kiểm tra nếu không thể mở file
+    ifstream in;
+    in.open(file_path);
+    if (!in.is_open()) { 
         cout << "Cannot open file" << endl;
         return;
     }
 
+    string header;              // xoa dong 1
+    getline(in, header);
+
     string line;
     string username, password;
 
-    while (getline(file, line)) {
+    while (getline(in, line)) {
         stringstream ss(line);
 
         getline(ss, username, ',');
         getline(ss, password, ',');
 
         User user = { username, password };
-        cout << user.username << user.password << std::endl;
+        cout << user.username << ", " << user.password << endl;
     }
-
-    file.close();
+    in.close();
 }
 
 void read_files(const path& path) {
-    for (const auto& entry :directory_iterator(path)) {
-        if (entry.is_directory()) {
-            read_files(entry.path());
+    string name;
+
+    for (const auto& entry : directory_iterator(path)) {
+        if (entry.is_directory()) {    // if is folder
+            name = entry.path().filename().string();
+            cout << name << endl;
+            read_files(entry.path());  // recursion
         }
         else {
-            // Kiểm tra tệp tin có phải là tệp tin csv hay không
-            if (entry.path().extension() == ".txt") {
-                cout << entry.path() << endl;
-                // Đọc và xử lý tệp tin csv
-                processCSVFile(entry.path());
-            }
+            name = entry.path().filename().stem().string();     // filename (-.txt)
+            cout << name << endl;
+            processCSVFile(entry.path());
         }
     }
 }
 
 int main() {
-    path root_path("School Year 2022-2023");
-    read_files(root_path);
+    path root_path("Data");
+    read_files(root_path);      // input all files in "Data";
     return 0;
 }
+
+/*
+ directory_iterator(path) : Hàm này nhận vào đường dẫn của một thư mục và trả về một iterator đại diện cho tất cả các tệp tin và thư mục trong thư mục đó.
+
+entry.is_directory() : Hàm này kiểm tra xem một entry có phải là một thư mục hay không.
+
+getline(file, header) và getline(ss, username, ',') : Hàm này đọc một dòng văn bản trong tệp tin.
+
+read_files(path) : Hàm này nhận vào đường dẫn của một thư mục và đọc tất cả các tệp tin và thư mục trong đó bằng cách đệ quy gọi chính nó với đường dẫn của các thư mục con.
+
+path root_path("Data") : Hàm này tạo một đối tượng path từ đường dẫn "Data".
+*/
