f976ddf946313b75d054c8779038cf49bb259285
nghednh <nghednh@gmail.com>
2023-04-21 14:46:15 +0700

    fix

--- System/System/Beginning.h
+++ System/System/Beginning.h
@@ -1,13 +1,9 @@
 #pragma once
 #include "Structure.h"
 
-
-void chooseRole();
-void login();
 void viewProfile(Staff* staff);
 void inputDate(Date& datee);
 void inputYear(SchoolYear*& year);
 void inputCourse(Course*& course);
-void addClasstoSchoolYear();
 void addClassToSchoolYear(Class* classs, SchoolYear* schoolyearr);
 void addCourseToSemester(Course* coursee, Semester* semester);
