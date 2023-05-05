55f350227119552a73c1afac6e78712089714a1f
nghednh <nghednh@gmail.com>
2023-05-04 17:08:31 -0700

    hàm login (remake)

--- System/System/login&menu.cpp
+++ System/System/login&menu.cpp
@@ -25,3 +25,59 @@ void UIlite() {
     cout << "|  <-  -> || courses.ctda.hcmus.edu.vn                                              |" << endl;
     cout << "|___________________________________________________________________________________|" << endl;
 }
+void login(int firstlog = 1) {
+    system("cls");
+    int role;
+    UI();
+    if (!firstlog) {
+        cout << "| Wrong password or username or role                                                |\n";
+        cout << "| Enter your role again:                                                            |\n";
+        cout << "| 1. Staff                                                                          |\n";
+        cout << "| 2. Student                                                                        |\n";
+        cout << "| 3. Exit                                                                           |\n";
+    }
+    else {
+        cout << "| Enter your role again:                                                            |\n";
+        cout << "| 1. Staff                                                                          |\n";
+        cout << "| 2. Student                                                                        |\n";
+        cout << "| 3. Exit                                                                           |\n";
+    }
+    cout << "| Choose: ";
+    cin >> role;
+    if (role != 3 && role != 2 && role != 1) {
+        login(0);
+    }
+    if (role == 3) {
+        system("cls");
+        UI();
+        cout << "| See you later!                                                                    |\n";
+        // Exit the program
+        return;
+    }
+    system("cls");
+    string username, password;
+    UI();
+    cout << "| Enter your username: ";
+    cin >> username;
+    cout << "| Enter your password: ";
+    cin >> password;
+    if (role == 1) {
+        if (checkStaffpassword(username, password)) {
+            menuStaff(username);
+        }
+        else {
+            system("cls");
+            login(0);
+        }
+    }
+    else if (role == 2) {
+        if (checkStudentpassword(username, password)) {
+            menuStudent(username);
+        }
+        else {
+            // Wrong password, prompt user to try again
+            system("cls");
+            login(0);
+        }
+    }
+}
