462873468df67f9b6377c80a43e7d19618326f00
nghednh <nghednh@gmail.com>
2023-05-04 17:12:44 -0700

    login&menu.h

--- System/System/login&menu.h
+++ System/System/login&menu.h
@@ -1,1 +1,9 @@
 #pragma once
+#include "Structure.h"
+bool checkStaffPassword(string, string);
+bool checkStudentPassword(string, string);
+void login(int);
+void menuStaff(string);
+void menuStudent(string);
+void UI();
+void UIlite();
