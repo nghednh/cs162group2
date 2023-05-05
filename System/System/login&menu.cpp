3e053426ce3fd71945edafc28abca265d6bb2608
nghednh <nghednh@gmail.com>
2023-05-04 17:06:47 -0700

    checkpassword giả

---
+++ System/System/login&menu.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+bool checkStudentpassword(string username, string password) {
+    return true;
+} /*=> cho cai ham cua ba vo, tai tui k chay duoc nen de vay*/
+bool checkStaffpassword(string username, string password) {
+    return  true;
+}
