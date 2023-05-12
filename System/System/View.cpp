7cddfeaccb45a8d40b70855b1d91d5848a5097fe
nghednh <nghednh@gmail.com>
2023-05-11 23:09:04 -0700

    view function

--- System/System/View.cpp
+++ System/System/View.cpp
@@ -2,7 +2,77 @@
 #include "login&menu.h"
 #include "Structure.h"
 
+void viewListClasses(SchoolYear* sYearHead)
+{
+	system("cls");
+	UIlite;
+	SchoolYear* sYearCur = sYearHead;
+	while (sYearCur)
+	{
+		Class* classCur = sYearCur->classHead;
+		while (classCur)
+		{
+			cout << classCur->name << endl;
+			classCur = classCur->classNext;
+		}
+		sYearCur = sYearCur->yearNext;
+	}
+}
 
+void viewListCourses(Course* courseHead)
+{
+	system("cls");
+	UIlite;
+	Course* courseCur = courseHead;
+	while (courseCur)
+	{
+		cout << courseCur->name << endl;
+		courseCur = courseCur->courseNext;
+	}
+}
+
+void viewStudentProfile(Student* stuCur);
+
+void viewStudentProfile(Student* stuCur)
+{
+	system("cls");
+	UIlite();
+	cout << "ID: " << stuCur->StuID << endl;
+	cout << "First name: " << stuCur->firstName << endl;
+	cout << "Last name: " << stuCur->lastName << endl;
+	cout << "Gender: " << stuCur->gender << endl;
+	cout << "Date of birth: " << stuCur->dateOfBirth.day << "/" << stuCur->dateOfBirth.month << "/" << stuCur->dateOfBirth.year << endl;
+	cout << "Social ID: " << stuCur->socialID << endl;
+	cout << "Curriculum: " << stuCur->curriculum << endl;
+	//	cout << "Class name: " << stuCur->className << endl;
+}
+
+void viewListStudentsInCourse(Course* courseCur)	// lop sinh hoat
+{
+	system("cls");
+	UIlite();
+	Student* stuCur = courseCur->stuHead->stuInClass;
+	while (stuCur)
+	{
+		//		viewStudentProfile(stuCur);
+		cout << stuCur->lastName << " " << stuCur->firstName << endl;
+		stuCur = stuCur->stuNext;
+	}
+}
+
+void viewListOfStudentInClass(Class* classCur)
+{
+	system("cls");
+	UIlite;
+	if (classCur == NULL) return;
+	Student* stuCur = classCur->stuHead;
+	while (stuCur)
+	{
+		//		viewStudentProfile(stuCur);
+		cout << stuCur->lastName << " " << stuCur->firstName << endl;
+		stuCur = stuCur->stuNext;
+	}
+}
 
 Student* findStudentByID_01(string ID, SchoolYear* sYearHead)
 {
